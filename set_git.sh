bd9e0cc92da541c3c9ad5820684eb235b84aa75a
nialldevlin <niallcdevlin@gmail.com>
2020-12-01 15:58:27 +0000

    Updated particle_filter.cpp

--- set_git.sh
+++ set_git.sh
@@ -4,28 +4,17 @@
 echo
 git pull
 echo
-
-# Ask the user for login details
-read -p 'Git repository url: ' upstreamVar
-read -p 'Git Username: ' userVar
-read -p 'Git email: ' emailVar
-
-echo
-echo Thank you $userVar!, we now have your credentials
-echo for upstream $upstreamVar. You must supply your password for each push.
-echo
-
 echo setting up git
 
-git config --global user.name $userVar
-git config --global user.email $emailVar
-git remote set-url origin $upstreamVar
+git config --global user.name nialldevlin 
+git config --global user.email niallcdevlin@gmail.com
+git remote set-url origin https://github.com/nialldevlin/CarND-Kidnapped-Vehicle-Project.git
 echo
 
-echo Please verify remote:
 git remote -v
-echo
 
-echo Please verify your credentials:
-echo username: `git config user.name`
-echo email: `git config user.email`
+read -p "Commit Message: " commitmsg
+
+git add .
+git commit -m "$commitmsg"
+git push origin master
